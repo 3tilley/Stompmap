f804ddbb72ad9b0a16a959e666d83475f4731ba1
Max Tilley <3tilley@googlemail.com>
2015-12-30 18:52:30 +0000

    Further changes

--- src/Stompmap/Controllers/AccountController.cs
+++ src/Stompmap/Controllers/AccountController.cs
@@ -60,7 +60,7 @@ namespace Stompmap.Controllers
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation(1, "User logged in.");
@@ -104,7 +104,7 @@ namespace Stompmap.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Username, Email = model.Username };
                 var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
